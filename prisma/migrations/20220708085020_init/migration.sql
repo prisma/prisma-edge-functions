bb1a68c32ccd314b9d21ed0acbc45f3e345eb519
ruheni <aleruheni@outlook.com>
2022-07-08 13:44:05 +0300

    update Prisma schema

---
+++ prisma/migrations/20220708085020_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Quote" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Quote_pkey" PRIMARY KEY ("id")
+);
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,70 +1,18 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import quotes from "./data.json"
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
-
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-]
-
-async function main() {
-  console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
+export async function main() {
+  console.log("[Elevator Music Cue] 🎸")
+  for (let quote of quotes) {
+    await prisma.quote.create({
+      data: {
+        author: quote.author,
+        content: quote.content
+      }
     })
-    console.log(`Created user with id: ${user.id}`)
   }
-  console.log(`Seeding finished.`)
+  console.log("Done 🎉")
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
